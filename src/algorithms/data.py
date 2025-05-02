9c79d55b74fe8d4d93f81e1a271fb19394790a6d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-05-02 21:16:59 +0100

    Add src/algorithms/data.py

---
+++ src/algorithms/data.py
@@ -0,0 +1,45 @@
+
+import cudf
+
+import src.elements.s3_parameters as s3p
+import src.elements.service as sr
+import src.elements.partitions as pr
+import src.s3.prefix
+
+class Data:
+
+    def __init__(self, service: sr.Service, s3_parameters: s3p.S3Parameters, arguments: dict):
+        """
+
+        :param service:
+        :param s3_parameters:
+        :param arguments:
+        """
+
+        self.__service = service
+        self.__s3_parameters = s3_parameters
+        self.__arguments = arguments
+
+        # An instance for interacting with objects within an Amazon S3 prefix
+        self.__bucket_name = self.__s3_parameters._asdict()[arguments['s3']['p_bucket']]
+        self.__pre = src.s3.prefix.Prefix(
+            service=self.__service,
+            bucket_name=self.__bucket_name)
+
+    def exc(self, partition: pr.Partitions) -> cudf.DataFrame:
+        """
+
+        :param partition:
+        :return:
+        """
+
+        listings = self.__pre.objects(prefix=partition.prefix.rstrip('/'))
+        keys = [f's3://{self.__bucket_name}/{listing}' for listing in listings]
+
+        blocks = [cudf.read_csv(filepath_or_buffer=key, header=0, usecols=['timestamp', 'ts_id', 'measure']) for key in keys]
+        block = cudf.concat(blocks)
+        block['datestr'] = cudf.to_datetime(block['timestamp'], unit='ms')
+        block['date'] = block['datestr'].dt.strftime('%Y-%m-%d')
+        block['date'] = cudf.to_datetime(block['date'])
+
+        return block[['date', 'measure']]
