d3b42860539834aea9fe9b289f9027e8580c07aa
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-05-03 01:27:03 +0100

    Update src/transfer/interface.py

--- src/transfer/interface.py
+++ src/transfer/interface.py
@@ -68,7 +68,6 @@ class Interface:
         strings = self.__get_metadata(frame=strings.copy())
         logging.info(strings)
 
-        '''
         # Prepare the S3 (Simple Storage Service) section
         src.transfer.cloud.Cloud(
             service=self.__service, s3_parameters=self.__s3_parameters).exc()
@@ -78,4 +77,3 @@ class Interface:
             service=self.__service, bucket_name=self.__s3_parameters.external).exc(
             strings=strings, tagging='project=hydrography')
         logging.info(messages)
-        '''
